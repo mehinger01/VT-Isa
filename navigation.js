73190b2030a90fedf37b4b186a892933cac243e9
mehinger01 <mike.ehinger01@gmail.com>
2026-07-31 12:06:45 -0400

    Add learning templates to navigation

--- navigation.js
+++ navigation.js
@@ -1,6 +1,7 @@
 const navItems = [
   ['Home', 'index.html'],
   ['Learning Lab', 'session-3-learning-lab.html'],
+  ['Learning Templates', 'learning-templates.html'],
   ['System Mapper', 'system-mapper.html'],
   ['Energy Dashboard', 'energy-dashboard.html'],
   ['Reset Builder', 'reset-builder.html'],
